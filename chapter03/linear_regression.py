e7299fbdd0c7cc9c84c3d51035c02ff274ce3b1f
tpeng <pengtaoo@gmail.com>
2012-11-26 15:40:25 +0800

    simple linear regression for chapter03

--- chapter03/linear_regression.py
+++ chapter03/linear_regression.py
@@ -1,24 +1,35 @@
 import numpy as np
 import pylab as pl
 
+# the simplest basis function
+def identity(x):
+  return x
+
 class Regression():
-  def __init__(self, x, y):
-    self._x = x
-    self._y = y
+  def __init__(self):
+    pass
+
+  def fit(self, x, y):
+    x0 = np.array(map(identity, x), float)
+    theta = x0.T
+    self.w = np.dot(1 / np.dot(theta.T, theta) * theta.T, y)
+    self.w0 = np.mean(y) - np.sum(map(identity, x[1:])) / (len(x) - 1)
 
   def predict(self, x):
-    return [1] * len(x)
+    pred = map(lambda x : self.w0 + identity(x), x)
+    return np.array(pred)
 
 if __name__ == '__main__':
   np.random.seed(5)
 
   # 100 data point
-  N = 1000
+  N = 100
   s = 10
 
   # the data to learn
   x = s * np.random.rand(N)
-  y = np.sin(x) + 0.1*np.random.randn(N)
+  w = np.random.rand()
+  y = w * x + 0.5 * np.random.randn(N)
 
   # plot the raw data (sin + noise)
   pl.plot(x,y,'.r')
@@ -26,7 +37,9 @@ if __name__ == '__main__':
   # predict with regression
   x0 = np.linspace(min(x),max(x),500)
 
-  m = Regression(x, y)
+  m = Regression()
+  m.fit(x, y)
+
   y0 = m.predict(x0)
 
   pl.plot(x0, y0)
