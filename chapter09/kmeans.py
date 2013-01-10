1fccaaaab09b7a6e6756e33f067691fd5e355ea9
tpeng <pengtaoo@gmail.com>
2013-01-10 11:23:52 +0800

    fix kmeans

--- chapter09/kmeans.py
+++ chapter09/kmeans.py
@@ -6,10 +6,18 @@ def distance(x1, x2):
 
 class Kmeans():
 
+    def __init__(self):
+        self.centroids = []
+
     def fix(self, xs, k):
+        prev_J= J = 0
+        tol = 1e-4
         n,d = xs.shape
         # uk is the centroid of k
         uk = np.zeros((k, d), float)
+
+        for i in range(k):
+            uk[i] = xs[np.random.randint(0, n)]
 
         converged = False
         while not converged:
@@ -25,13 +33,27 @@ class Kmeans():
             for (i, j) in enumerate(distances.argmin(axis=1)):
                 rnk[i, j] = 1
 
-            print rnk[0:10]
             # fix rnk and minimize with uk
             # we select to the means of all the point belonging to centroid k
             for j in range(k):
-                uk[j] = (xs[rnk == 1]).mean()
+                x = []
+                for i in range(n):
+                    if rnk[i, j] == 1:
+                        x.append(xs[i])
+                uk[j] = np.array(x).mean(axis=0)
 
-            print uk
+            J = 0
+            for i in range(n):
+                for j in range(k):
+                    J += rnk[i,j] * distance(xs[i], uk[j])
+
+            if abs(J-prev_J) < tol:
+                converged=True
+            else:
+                prev_J = J
+
+        print 'converged. '
+        self.centroids = uk
 
 def read_data(name):
     xs = []
@@ -39,14 +61,14 @@ def read_data(name):
         xs.append(map(float, line.rstrip().split()))
     return np.array(xs)
 
-def plot(xs):
+def plot(xs, centroids):
     plt.scatter(xs[:,0], xs[:,1])
+    plt.scatter(centroids[:,0], centroids[:,1], color='r')
     plt.show()
 
 if __name__ == '__main__':
     xs = read_data('../dataset/faithful.dat')
-    x1 = [1, 2]
-    x2 = [3, 3]
-    #    plot(xs)
     kmeans = Kmeans()
-    kmeans.fix(xs, 4)
+    kmeans.fix(xs, 2)
+    print kmeans.centroids
+    plot(xs, kmeans.centroids)
